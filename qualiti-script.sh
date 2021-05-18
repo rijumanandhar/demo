e301c4e447df551be2056e7ae6fa0cec47262941
rmanandhar@sevadev.com <rmanandhar@sevadev.com>
2021-05-18 15:24:40 +0545

    changes 16

--- qualiti-script.sh
+++ qualiti-script.sh
@@ -3,12 +3,12 @@
  
 set -ex
  
-PROJECT_ID='332'
+PROJECT_ID='333'
 API_KEY='iQvL1dkQy9CAPb3jvlhR1AHHCFLd1QfTSjkPPH40'
 CLIENT_ID='a7d8412bfe5d40a69b9be22fc38b309a'
 SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
 API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
-INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjI1MTIsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiSmVua2lucyIsIm9wdGlvbnMiOnt9LCJpYXQiOjE2MTYzOTY1MzF9.o5YcD1aRb8_nCuPBMR5H9jU_NgtlID7X_3TXgGRBfuU'
+INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzMsImFwaV9rZXlfaWQiOjQ4MjIsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiZ2l0bGFiIiwib3B0aW9ucyI6e30sImlhdCI6MTYyMTI0Mjg5Mn0.ZE9D7MxlCQV0LwpbJldKu7ZX_A7YFZzbdXQgD6v2e9A'
 INTEGRATIONS_API_URL='https://72337d6507f8.ngrok.io'
  
 sudo apt-get update -y
