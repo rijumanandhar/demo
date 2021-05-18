659a28f92eb29e7c9882b94a424d2db839c5d83c
rmanandhar@sevadev.com <rmanandhar@sevadev.com>
2021-05-18 15:06:42 +0545

    changes 15

--- qualiti-script.sh
+++ qualiti-script.sh
@@ -16,7 +16,7 @@ sudo apt-get install -y jq
  
 #Trigger test run
 TEST_RUN_ID="$( \
-  curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/gitlab/${PROJECT_ID}/events \
+  curl -X POST -G ${INTEGRATIONS_API_URL}/integrations/gitlab/${PROJECT_ID}/events \
     -d 'token='$INTEGRATION_JWT_TOKEN''\
     -d 'projectId='$PROJECT_ID''\
     -d 'triggerType=Deploy'\
