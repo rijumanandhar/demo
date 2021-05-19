74eb4d63ebd1fe963feca63d6d736bbb926e5c97
rmanandhar@sevadev.com <rmanandhar@sevadev.com>
2021-05-19 11:28:17 +0545

    changes 19

--- qualiti-script.sh
+++ qualiti-script.sh
@@ -3,20 +3,20 @@
  
 set -ex
  
-PROJECT_ID='332'
+PROJECT_ID='333'
 API_KEY='iQvL1dkQy9CAPb3jvlhR1AHHCFLd1QfTSjkPPH40'
 CLIENT_ID='a7d8412bfe5d40a69b9be22fc38b309a'
 SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
 API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
-INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjI1MTIsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiSmVua2lucyIsIm9wdGlvbnMiOnt9LCJpYXQiOjE2MTYzOTY1MzF9.o5YcD1aRb8_nCuPBMR5H9jU_NgtlID7X_3TXgGRBfuU'
-INTEGRATIONS_API_URL='http://70943036ab85.ngrok.io'
+INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzMsImFwaV9rZXlfaWQiOjQ4MjIsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiZ2l0bGFiIiwib3B0aW9ucyI6e30sImlhdCI6MTYyMTI0Mjg5Mn0.ZE9D7MxlCQV0LwpbJldKu7ZX_A7YFZzbdXQgD6v2e9A'
+INTEGRATIONS_API_URL=' http://70943036ab85.ngrok.io'
  
 sudo apt-get update -y
 sudo apt-get install -y jq
  
 #Trigger test run
 TEST_RUN_ID="$( \
-  curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/gitlab/${PROJECT_ID}/events \
+  curl -X POST -G ${INTEGRATIONS_API_URL}/integrations/gitlab/${PROJECT_ID}/events \
     -d 'token='$INTEGRATION_JWT_TOKEN''\
     -d 'projectId='$PROJECT_ID''\
     -d 'triggerType=Deploy'\
